9918cd85159991bef3f11b3a65f73f98b56fe89d
Jedron-Moise <mosesnayaga6@gmail.com>
2025-06-26 21:35:09 +0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,63 +1,60 @@
-#define STEP_PIN 7
-#define DIR_PIN 6
-#define MS1_PIN 3
-#define MS2_PIN 4
-#define MS3_PIN 5
-#define ENABLE_PIN 2
-#define etat digitalRead(DIR_PIN)
-#define changeDirection digitalWrite(DIR_PIN, !etat)
-int ldr1 = A0;
-int ldr2 = A1;
+// Declaration of pins
+int IN1 = 4;
+int IN2 = 3;
+int ldr1 = A4;
+int ldr2 = A5;
 int ldr3 = A2;
 int ldr4 = A3;
-int gaucheHaut = 0;
-int droitHaut = 0;
-int gaucheBas = 0;
-int droiteBas = 0;
+//int ldr5 = A4;
+int gauche_haut = 0;
+int gauche_bas = 0;
+int droite_haut = 0;
+int droite_bas = 0;
+//int milieu = 0;
+int seuil_1 = 20;// threshold to activate the actuator
+int seuil_2 = 700; // difference between day and night
 void setup() {
   Serial.begin(9600);
-  Serial.println("Test A4988");
-  pinMode(STEP_PIN, OUTPUT);
-  pinMode(DIR_PIN, OUTPUT);
-  pinMode(MS1_PIN, OUTPUT);
-  pinMode(MS2_PIN, OUTPUT);
-  pinMode(MS3_PIN, OUTPUT);
-  pinMode(ENABLE_PIN, OUTPUT);
-
+  Serial.println("Test A4988");// allows you to read the value on the LDRs
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
   pinMode(ldr1, INPUT);
   pinMode(ldr2, INPUT);
   pinMode(ldr3, INPUT);
   pinMode(ldr4, INPUT);
-
-
-  digitalWrite(DIR_PIN, LOW);
-  digitalWrite(MS1_PIN, LOW);
-  digitalWrite(MS2_PIN, LOW);
-  digitalWrite(MS3_PIN, LOW);
-  digitalWrite(ENABLE_PIN, LOW);
-
+  //pinMode(ldr5, INPUT);
 }
-int stopper = 0;
-void loop() {
-  gaucheHaut = analogRead(ldr1);
-  Serial.println(gaucheHaut);
-  gaucheBas = analogRead(ldr2);
-  Serial.println(gaucheBas);
-  if (gaucheHaut < 1000)
-    digitalWrite(ENABLE_PIN, LOW);
-  else {
-    for (int i = 0; i < 800; i++) {
-      digitalWrite(STEP_PIN, 1);
-      delayMicroseconds(800);
-      digitalWrite(STEP_PIN, 0);
-      delayMicroseconds(800);
-    }
-  }
-  /*right = analogRead(ldr2);
-    Serial.println(right);
-    top = analogRead(ldr3);
-    Serial.println(top);
-    bottom = analogRead(ldr4);
-    Serial.println(bottom);*/
 
+void loop() {
+  // Lire la valeur reçue des LDR
+  gauche_haut = analogRead(ldr1);
+  Serial.println(gauche_haut+gauche_bas);
+  gauche_bas = analogRead(ldr2);
+  //Serial.println(gauche_bas);
+  droite_haut = analogRead(ldr3);
+  Serial.println(droite_haut+droite_bas);
+  droite_bas = analogRead(ldr4);
+  //Serial.println(droite_bas);
+  //milieu = analogRead(ldr5);
+  //Serial.println(milieu);
+  // Comparer et actionner le verin
+  if (((gauche_haut + gauche_bas) - (droite_haut + droite_bas)) > seuil_1) {
+    digitalWrite(IN1, 0);
+    digitalWrite(IN2, 1);
+    //delay(1000);
+
+  }
+
+  else if (((droite_haut + droite_bas) - (gauche_haut + gauche_bas)) > seuil_1) {
+    digitalWrite(IN1, 1);
+    digitalWrite(IN2, 0);
+    //delay(1000);
+
+  }
+
+  else {
+    digitalWrite(IN1, 0);
+    digitalWrite(IN2, 0);
+    //delay(1000);
+  }
 }
